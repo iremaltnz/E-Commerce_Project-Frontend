a1c4a9f73e1be07f2eac10eb445248d06f813ea2
Mehmet Karasu <mkarasu416@gmail.com>
2021-08-13 19:05:54 +0300

    Modeller Oluşturuldu

---
+++ ECommers/src/app/models/Product.ts
@@ -0,0 +1,10 @@
+export interface Product{
+    productId:number;
+    categoryId:number;
+    brandId:number;
+    colorId:number;
+    productName:string;
+    stockQuantity:number;
+    productPrice:number;
+    description:string;
+} 
